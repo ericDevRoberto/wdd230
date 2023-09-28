2eb3ce0842738cd45da3c0c69141a0bfa16380b6
Eric Roberto <58601124+ericDevRoberto@users.noreply.github.com>
2023-09-28 18:28:32 -0300

    HAMBURGER

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -7,3 +7,12 @@ let lastModification = Date.parse(document.lastModified);
 let lastDate = new Date(lastModification)
 document.querySelector(".footer-date").innerHTML = `Last Modification: ${lastDate.toLocaleString('sv')}`
 
+const hamButton = document.querySelector('#menuHamburger');
+const navigation = document.querySelector('.vertical-menu');
+const mapContainer = document.querySelector('.menu-map-container');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+	mapContainer.classList.toggle('open');
+});
